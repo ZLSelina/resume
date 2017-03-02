fad0e7396dbf8a04aa235d8356fbcc4fc8d94b4f
ZLSelina <779006219@qq.com>
2017-03-02 17:51:55 +0800

    update20170302

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 ~(function(){
     var musicBox=document.getElementById('music'),
         music=musicBox.getElementsByTagName('audio')[0];
+
     window.setTimeout(function(){
         music.play();
         musicBox.className='music musicMove';
@@ -9,9 +10,11 @@
         if(music.paused){
             music.play();
             musicBox.className='music musicMove';
+            musicBox.style.animationPlayState='running';
         }else {
             music.pause();
-            musicBox.className='music';
+            // musicBox.className='music';
+            musicBox.style.animationPlayState='paused';
         }
     });
 }());
